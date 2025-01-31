aeab1edad2b1a3a80df3897a597403d12babc2ac
josmyrose-03 <josmyrosemathew02@gmail.com>
2025-01-31 14:41:18 +0530

    request-service completed

--- src/main/java/com/capstone/request_service/service/RequestService.java
+++ src/main/java/com/capstone/request_service/service/RequestService.java
@@ -2,13 +2,25 @@ package com.capstone.request_service.service;
 
 import com.capstone.request_service.entity.RequestEntity;
 import com.capstone.request_service.exception.ResourceNotFoundException;
+import com.capstone.request_service.pojo.CommunityMembershipPojo;
+import com.capstone.request_service.pojo.CommunityMembershipUpdateAmountPojo;
+import com.capstone.request_service.pojo.CommunityPojo;
+import com.capstone.request_service.pojo.DateTimePojo;
+import com.capstone.request_service.pojo.RequestFilterByCommunityIdAndEmailInputPojo;
+import com.capstone.request_service.pojo.RequestFilterByCommunityIdAndStartDateTimeRangeInputPojo;
+import com.capstone.request_service.pojo.RequestFilterByCommunityIdInputPojo;
+import com.capstone.request_service.pojo.RequestInputPojo;
 import com.capstone.request_service.pojo.RequestPojo;
+import com.capstone.request_service.pojo.TransactionInputPojo;
+import com.capstone.request_service.pojo.TransactionOutputPojo;
+import com.capstone.request_service.pojo.UserOutputDataPojo;
 import com.capstone.request_service.repository.RequestRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,14 +32,17 @@ public class RequestService {
     private RequestPojo convertEntityToPojo(RequestEntity requestEntity) {
         RequestPojo requestPojo = new RequestPojo();
         BeanUtils.copyProperties(requestEntity, requestPojo);
-        return requestPojo;
-    }
+        RestClient restClient = RestClient.create();
+        UserOutputDataPojo responseUser = restClient.get()
+                .uri("http://localhost:5001/api/users/email/" + requestEntity.getEmail())
+                .retrieve().body(UserOutputDataPojo.class);
+        requestPojo.setUser(responseUser);
+        CommunityPojo responseCommunity = restClient.get()
+                .uri("http://localhost:5002/api/communities/" + requestEntity.getCommunityId())
+                .retrieve().body(CommunityPojo.class);
+        requestPojo.setCommunity(responseCommunity);
 
-    public RequestPojo addRequest(RequestPojo requestPojo) {
-        RequestEntity requestEntity = new RequestEntity();
-        BeanUtils.copyProperties(requestPojo, requestEntity);
-        RequestEntity savedEntity = requestRepository.save(requestEntity);
-        return convertEntityToPojo(savedEntity);
+        return requestPojo;
     }
 
     // Get All Requests
@@ -47,9 +62,9 @@ public class RequestService {
         return convertEntityToPojo(requestEntity);
     }
 
-    // Get Requests by Username
-    public List<RequestPojo> getRequestsByUsername(String username) {
-        List<RequestEntity> requestEntities = requestRepository.findByUsername(username);
+    // Get Requests by Email
+    public List<RequestPojo> getRequestsByEmail(String email) {
+        List<RequestEntity> requestEntities = requestRepository.findByEmail(email);
         List<RequestPojo> requestPojos = new ArrayList<>();
         for (RequestEntity requestEntity : requestEntities) {
             requestPojos.add(convertEntityToPojo(requestEntity));
@@ -59,7 +74,7 @@ public class RequestService {
 
     // Get Requests by Community ID
     public List<RequestPojo> getRequestsByCommunityId(int communityId) {
-        List<RequestEntity> requestEntities = requestRepository.findByCommunityId(communityId);
+        List<RequestEntity> requestEntities = requestRepository.findByCommunityIdAndStatus(communityId, "pending");
         List<RequestPojo> requestPojos = new ArrayList<>();
         for (RequestEntity requestEntity : requestEntities) {
             requestPojos.add(convertEntityToPojo(requestEntity));
@@ -67,9 +82,9 @@ public class RequestService {
         return requestPojos;
     }
 
-    // Get Requests by Status
-    public List<RequestPojo> getRequestsByStatus(String status) {
-        List<RequestEntity> requestEntities = requestRepository.findByStatus(status);
+    public List<RequestPojo> getByStatusAndCommunityId(RequestFilterByCommunityIdInputPojo requestDataPojo) {
+        List<RequestEntity> requestEntities = requestRepository.findByStatusAndCommunityId(requestDataPojo.getStatus(),
+                requestDataPojo.getCommunityId());
         List<RequestPojo> requestPojos = new ArrayList<>();
         for (RequestEntity requestEntity : requestEntities) {
             requestPojos.add(convertEntityToPojo(requestEntity));
@@ -77,41 +92,88 @@ public class RequestService {
         return requestPojos;
     }
 
-    // Update Request Status
-    public RequestPojo updateRequestStatus(int requestId, String status) {
-        RequestEntity requestEntity = requestRepository.findById(requestId)
-                .orElseThrow(() -> new ResourceNotFoundException("Request not found with id " + requestId));
-        requestEntity.setStatus(status);
+    public List<RequestPojo> getByStatusAndCommunityIdAndEmail(
+            RequestFilterByCommunityIdAndEmailInputPojo requestDataPojo) {
+        List<RequestEntity> requestEntities = requestRepository.findByStatusAndCommunityIdAndEmail(
+                requestDataPojo.getStatus(),
+                requestDataPojo.getCommunityId(), requestDataPojo.getEmail());
+        List<RequestPojo> requestPojos = new ArrayList<>();
+        for (RequestEntity requestEntity : requestEntities) {
+            requestPojos.add(convertEntityToPojo(requestEntity));
+        }
+        return requestPojos;
+    }
+
+    public List<RequestPojo> getAllByCommunityIdAndStatusAndRequestDateTimeBetween(
+            RequestFilterByCommunityIdAndStartDateTimeRangeInputPojo requestDataPojo) {
+        List<RequestEntity> requestEntities = requestRepository.findAllByCommunityIdAndStatusAndRequestDateTimeBetween(
+                requestDataPojo.getCommunityId(),
+                requestDataPojo.getStatus(),
+                requestDataPojo.getStartDate(),
+                requestDataPojo.getEndDate());
+        List<RequestPojo> requestPojos = new ArrayList<>();
+        for (RequestEntity requestEntity : requestEntities) {
+            requestPojos.add(convertEntityToPojo(requestEntity));
+        }
+        return requestPojos;
+    }
+
+    // Add Request
+    @SuppressWarnings("null")
+    public RequestPojo addRequest(RequestInputPojo requestPojo) {
+        RequestEntity requestEntity = new RequestEntity();
+        BeanUtils.copyProperties(requestPojo, requestEntity);
+
+        RestClient restClient = RestClient.create();
+        DateTimePojo dateTimePojo = restClient.put().uri("http://localhost:5010/api/time")
+                .body(new DateTimePojo(0, LocalDateTime.now())).retrieve().body(DateTimePojo.class);
+        requestEntity.setRequestDateTime(dateTimePojo.getDateTime());
+
+        requestEntity.setStatus("pending");
+         
+        if(getByStatusAndCommunityIdAndEmail(new RequestFilterByCommunityIdAndEmailInputPojo(requestPojo.getEmail(), requestPojo.getCommunityId(), "pending")).size() == 0){
+            RequestEntity savedEntity = requestRepository.saveAndFlush(requestEntity);
+            return convertEntityToPojo(savedEntity);
+        }
+        return null;
+    }
+
+    // Update Request Status To Rejected
+    public RequestPojo updateRequestStatusToRejected(int requestId) {
+        RequestEntity requestEntity = requestRepository.findById(requestId).orElse(null);
+        requestEntity.setStatus("rejected");
         RequestEntity updatedEntity = requestRepository.save(requestEntity);
         return convertEntityToPojo(updatedEntity);
     }
 
-    // Delete Request
-    public void deleteRequest(int requestId) {
-        if (!requestRepository.existsById(requestId)) {
-            throw new ResourceNotFoundException("Request not found with id " + requestId);
+    // Update Request Status To Approved
+    public RequestPojo updateRequestStatusToApproved(int requestId) {
+        RequestEntity requestEntity = requestRepository.findById(requestId).orElse(null);
+        requestEntity.setStatus("approved");
+        TransactionInputPojo newTransaction = new TransactionInputPojo(requestEntity.getEmail(),
+                requestEntity.getCommunityId(), "Debit", requestEntity.getAmount(), 0);
+        // localhost:5006/api/transactions
+        RestClient restClient = RestClient.create();
+        CommunityPojo responseCommunity = restClient.get()
+                .uri("http://localhost:5002/api/communities/" + requestEntity.getCommunityId())
+                .retrieve().body(CommunityPojo.class);
+        if (responseCommunity != null && responseCommunity.getCurrentAmount() >= requestEntity.getAmount()) {
+            CommunityMembershipUpdateAmountPojo communityMembershipUpdateAmountPojo = new CommunityMembershipUpdateAmountPojo(requestEntity.getCommunityId(), requestEntity.getEmail(), 0);
+            restClient.put()
+                .uri("http://localhost:5005/api/CommunityMembership/setLoanTaken")
+                .body(communityMembershipUpdateAmountPojo)
+                .retrieve().body(CommunityMembershipPojo.class);
+                
+            restClient.post()
+                    .uri("http://localhost:5006/api/transactions")
+                    .body(newTransaction)
+                    .retrieve().body(TransactionOutputPojo.class);
+            requestEntity = requestRepository.save(requestEntity);
+            return convertEntityToPojo(requestEntity);
+        } else {
+            return null;
         }
-        requestRepository.deleteById(requestId);
-    }
 
-    // Get Requests Sorted by Date
-    public List<RequestPojo> getRequestsSortedByDate() {
-        List<RequestEntity> requestEntities = requestRepository.findAll(Sort.by(Sort.Order.asc("requestDate")));
-        List<RequestPojo> requestPojos = new ArrayList<>();
-        for (RequestEntity requestEntity : requestEntities) {
-            requestPojos.add(convertEntityToPojo(requestEntity));
-        }
-        return requestPojos;
-    }
-
-    // Get Requests Pending Approval
-    public List<RequestPojo> getRequestsPendingApproval(String status) {
-        List<RequestEntity> requestEntities = requestRepository.findByStatus(status);
-        List<RequestPojo> requestPojos = new ArrayList<>();
-        for (RequestEntity requestEntity : requestEntities) {
-            requestPojos.add(convertEntityToPojo(requestEntity));
-        }
-        return requestPojos;
     }
 
 }
